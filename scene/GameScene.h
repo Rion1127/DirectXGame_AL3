a1e8bcb5f56ac8d81479a45a7c551e3577889f57
LC1A_09_suzuki <k021g1127@g.neec.ac.jp>
2022-04-18 15:08:12 +0900

    02_02_確認課題

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -55,7 +55,7 @@ class GameScene {
 	//3Dモデル
 	Model* model_ = nullptr;
 	//ワールドトランスフォーム
-	WorldTransform worldTransform_;
+	WorldTransform worldTransform_[100];
 	//ビュープロジェクション
 	ViewProjection viewProjection_;
 	//サウンドハンドルデータ
@@ -64,7 +64,8 @@ class GameScene {
 	uint32_t voiceHandle_ = 0;
 	//値を表示したい変数
 	int32_t value_ = 0;
-
+	//カメラ上方向の角度
+	float viewAngle = 0.0f;
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
